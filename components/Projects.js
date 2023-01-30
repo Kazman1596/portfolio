94e1e751700ab95792115c89864a8ed023f59022
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-01-30 10:13:14 -0700

    first version of portfolio with dummy data

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
         <div id='projects' className='container-layout'>
             <div className='w-full h-full md:ml-[256px]'>
                 <h1 className='t-0 p-[30px] border-b-2 border-sky-700 text-4xl text-sky-700'>projects.</h1>
-                <div className='m-5 h-full max-h-[800px] grid md:grid-cols-2 lg:grid-cols-3 gap-5 overflow-scroll'>
+                <div className='m-5 max-h-[800px] grid md:grid-cols-2 lg:grid-cols-3 gap-5 overflow-scroll'>
                     <Project id={travelBlog.id} img={travelBlog.img} title={travelBlog.title} description={travelBlog.description} />
                     <Project id={festivalFinder.id} img={festivalFinder.img} title={festivalFinder.title} description={festivalFinder.description} />
                     <Project id={barBack.id} img={barBack.img} title={barBack.title} description={barBack.description} />
--- project_data/project_data.js
+++ project_data/project_data.js
@@ -1,15 +1,5 @@
 const aboutParagraph = `
-        My name is Stephen Kaczmarowski and I graduated Northern Arizona University with a
-        Bachelor's in Microbiology and minoring in Chemistry. During COVID, I started
-        learning HTML, CSS, and JavaScript through Udemy, and quickly enveloped my time
-        learning these technologies.
-
-        I have always been a creator at heart; woodworking, music production, and software
-        development is no different. I love being able to create projects, applications, and
-        work through my code like a puzzle, making sure each piece is in place.
-
-        There is still so much to learn, and I am excited to grow my skills in tech for
-        a long time.
+        My name is Stephen Kaczmarowski.
     `
 ;
 
@@ -19,9 +9,7 @@ const travelBlog= {
     title: 'Travel Blog',
     link: '/',
     description: `
-        This is a C.R.U.D. application utilizing Google Firebase. Maybe
-        make it a point to look at LSDream's website for reference. What
-        a cool vibe.
+        This is a C.R.U.D. application utilizing Google Firebase.
     `,
 }
 
