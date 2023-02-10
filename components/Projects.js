4f116bb48d323f9bd0ed70c3209c7128735d751b
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-02-10 00:35:54 -0700

    fixed project overlap bug

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
         <div id='projects' className='container-layout relative'>
             <div className='w-full h-full md:ml-[256px]'>
                 <h1 className='t-0 p-[30px] border-b-2 border-sky-700 text-4xl text-sky-700'>projects.</h1>
-                <div className='m-5 max-h-[800px] grid md:grid-cols-2 lg:grid-cols-3 gap-5 overflow-y-scroll'>
+                <div className='m-5 max-h-[80vh] grid md:grid-cols-2 lg:grid-cols-3 gap-5 overflow-y-scroll'>
                     <Project id={videoSurf.id} img={videoSurf.img} gif={videoSurf.gif} title={videoSurf.title} description={videoSurf.description} />
                     <Project id={artistBlog.id} img={artistBlog.img} gif={artistBlog.gif} title={artistBlog.title} description={artistBlog.description} />
                     <Project id={instaConnect.id} img={instaConnect.img} gif={instaConnect.gif} title={instaConnect.title} description={instaConnect.description} />
